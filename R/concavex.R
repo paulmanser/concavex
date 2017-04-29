6ed05be479ed659e4fc822adeebc9a4e998b3205
Paul Manser <paulmanser26@gmail.com>
2017-04-28 17:08:19 -0700

    commit .R file for package man page

---
+++ R/concavex.R
@@ -0,0 +1,11 @@
+#' concavex: A package for Bayesian dose-response modeling
+#'
+#' Provide even more details here!
+#' 
+#' 
+#' @section concavex functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name concavex
+NULL
