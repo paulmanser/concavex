0cc89b6ae38a6e1f63583ce5b42836ebd8bdc5bf
Paul Manser <paulmanser26@gmail.com>
2017-04-28 17:07:10 -0700

    a little tidying

--- R/ccvx_build_jags.R
+++ R/ccvx_build_jags.R
@@ -1,6 +1,6 @@
 #' Build Concavex JAGS code
 #' 
-#' This function creates the JAGS code within R for fitting the Concavex model. Will eventually allow for flexible specification of priors
+#' This function creates JAGS code within R for fitting the Concavex model. Will eventually allow for flexible specification of priors and extracting DDCPs, predictive probabilities, etc.
 #' 
 #' @export
 #' @examples 
@@ -33,15 +33,13 @@ ccvx_build_jags <- function() {
     
     # transformations --------------------------------------------------------
 
+    ## posterior credible intervals for d-r curve
     for(ii in 1:length(pred.doses)){
-      
-      ## posterior credible interval for d-r curve
       mu.tilde[ii] <- theta_0 + theta_1 * (((((1-lambda)/2)^2 + lambda)/(((1-lambda)/2)^2 + lambda*pred.doses[ii]))*pred.doses[ii])
       trt.post[ii] <- theta_1 * (((((1-lambda)/2)^2 + lambda)/(((1-lambda)/2)^2 + lambda*pred.doses[ii]))*pred.doses[ii])
-      
     }
     
-    ## posterior distns for actual doses tested
+    ## posterior distns for doses tested
     for(ii in 1:length(dose)){
       dose.post[ii] <- theta_0 + theta_1 * (((((1-lambda)/2)^2 + lambda)/(((1-lambda)/2)^2 + lambda*dose[ii]))*dose[ii])
     }
