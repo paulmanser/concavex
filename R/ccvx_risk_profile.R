12e0d1cb20c656954bd615ebd243d493eacbbd5a
Paul Manser <paulmanser26@gmail.com>
2017-05-01 16:59:48 -0700

    initial commit of risk profile function

---
+++ R/ccvx_risk_profile.R
@@ -0,0 +1,78 @@
+#' Plot Concavex model risk profiles 
+#' 
+#' Function to plot Concavex model risk profiles by effect size and dose
+#' 
+#' @param ccvx.samples List containg Gibbs samples output by ccvx_fit() function
+#' 
+#' @export
+#' @examples 
+#' ccvx.mod <- ccvx_build_jags()
+#' ccvx.samples <- ccvx_fit(ccvx.mod, doses = 0:4, mu.hat = c(1, 20, 50, 60, 65), stderr = rep(10, 5))
+#' par(mfrow=c(1,2))
+#' ccvx_plot_fit(ccvx.samples, placebo.adjusted = FALSE)
+#' ccvx_plot_fit(ccvx.samples, placebo.adjusted = TRUE)
+
+
+
+ccvx_risk_profile <- function(ccvx.samples, eff.thresholds = NULL) {
+  
+  # if(!is.na(eff.thresholds)) {
+    # dose prob plot fix eff ---------------------------------------------------------------
+    eff.mat <- matrix(nr = dim(ccvx.samples$jags.samples$trt.post)[1],
+                      nc = length(eff.thresholds))
+    
+    for(jj in 1:ncol(eff.mat)) {
+      for(ii in 1:nrow(eff.mat)) {
+        eff.mat[ii, jj] <- mean(unlist(ccvx.samples$jags.samples$trt.post[ii, , ]) > eff.thresholds[jj])
+      }
+    }
+  
+    plot(0, type = 'n', ylim = c(0, 1), xlim = range(ccvx.samples$doses),
+         ylab = "Probability",
+         main = "Probability of Exceeding Various Efficacy Thresholds \n by Threshold",
+         xlab = "Dose")
+    grid(lwd = 2)
+    
+    for(jj in 1:ncol(eff.mat)) {
+      lines(seq(min(ccvx.samples$doses), max(ccvx.samples$doses), length.out = nrow(eff.mat)),
+            eff.mat[, jj],
+            col = jj, lwd = 2)
+    }
+  
+    legend("bottomright",
+           legend = eff.thresholds,
+           fill = 1:length(eff.thresholds), border = NA)
+  # }
+  
+
+  
+  # p for tested doses ------------------------------------------------------
+   
+  doses <- ccvx.samples$doses
+  trt.eff.range <- seq(0, quantile(ccvx.samples$jags.samples$theta_1, probs = .95), length.out = 250)
+
+  eff.mat <- matrix(nr = length(trt.eff.range), nc = length(doses))
+
+  for(jj in 1:ncol(eff.mat)) {
+    for(ii in 1:nrow(eff.mat)) {
+      eff.mat[ii, jj] <- mean(unlist(ccvx.samples$jags.samples$dose.post[jj, , ]) > trt.eff.range[ii])
+    }
+  }
+
+  plot(0, type = 'n', xlim = range(trt.eff.range), ylim = c(0, 1), cex.lab = 1.4,
+       ylab = "Probability",
+       main = "Probability of Exceeding Various Efficacy Thresholds \n by Dose",
+       xlab = "Treatment Effect over Placebo")
+  
+  grid(lwd=2)
+
+  for(jj in 1:ncol(eff.mat)) {
+    lines(trt.eff.range, eff.mat[, jj], col = jj, lwd = 2)
+  }
+
+  legend("bottomleft", cex = 1.3, legend= doses, fill = 1:length(doses), border = NA)
+
+}
+
+
+
