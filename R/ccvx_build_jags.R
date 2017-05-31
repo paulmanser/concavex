021ff4e55ec3698c9f2fbee9c47d7fd6618e419d
Paul Manser <paulmanser26@gmail.com>
2017-05-31 14:09:24 -0700

    add in JAGS code to compute predictive probabilities

--- R/ccvx_build_jags.R
+++ R/ccvx_build_jags.R
@@ -5,6 +5,7 @@
 #' @param prior.theta_0  Prior for theta_0 specified in terms of JAGS code. The default is a non-informative normal prior: 'theta_0 ~ dnorm(0, 1E-4)'
 #' @param prior.theta_1  Prior for theta_1 specified in terms of JAGS code. The default is a non-informative normal prior: 'theta_1 ~ dnorm(0, 1E-4)'
 #' @param prior.lambda   Prior for lambda specified in terms of JAGS code. The default is a non-informative uniform prior: 'lambda ~ dunif(-.999, .999)' 
+#' @param predictive.probs  Indicator for whether to compute posterior predictive probabilites for a future study. default is FALSE. if set to TRUE, the ccvx_fit function requires additional inputs
 #' 
 #' @export
 #' @examples 
@@ -14,7 +15,29 @@
 
 ccvx_build_jags <- function(prior.theta_0 = "theta_0 ~ dnorm(0, 1E-4)", 
                             prior.theta_1 = "theta_1 ~ dnorm(0, 1E-4)", 
-                            prior.lambda = "lambda ~ dunif(-.999, .999)") {
+                            prior.lambda = "lambda ~ dunif(-.999, .999)",
+                            predictive.probs = FALSE) {
+  
+  if(predictive.probs){
+    pred.prob.code <- 
+    "
+    pred.pbo ~ dnorm(theta_0, tau.ph3)
+      
+    for(ii in 1:length(pred.doses)){
+      mu.tilde.pred[ii] ~ dnorm(mu.tilde[ii], tau.ph3)
+      trt.post.pred[ii] <- mu.tilde.pred[ii] - pred.pbo
+    }
+    
+    for(ii in 1:length(dose)){
+      mu.tilde.pred.dose[ii] ~ dnorm(dose.post[ii], tau.ph3)
+      trt.post.pred.dose[ii] <- mu.tilde.pred.dose[ii] - pred.pbo
+    }
+    "
+  }else{
+    pred.prob.code <- ""
+  }
+  
+  
   paste0(
   "
   model{
@@ -49,8 +72,13 @@ ccvx_build_jags <- function(prior.theta_0 = "theta_0 ~ dnorm(0, 1E-4)",
     ## posterior distns for doses tested
     for(ii in 1:length(dose)){
       dose.post[ii] <- theta_0 + theta_1 * (((((1-lambda)/2)^2 + lambda)/(((1-lambda)/2)^2 + lambda*dose[ii]))*dose[ii])
+      trt.post.dose[ii] <- theta_1 * (((((1-lambda)/2)^2 + lambda)/(((1-lambda)/2)^2 + lambda*dose[ii]))*dose[ii])
     }
-    
+  ",
+  pred.prob.code,
+  
+  
+  "
   }
   "
   )
