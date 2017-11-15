2926592d464739bcca2b4fde6e69b10fb07b5cb2
Paul Manser <paulmanser26@gmail.com>
2017-11-15 09:27:26 -0800

    generalize to ccvx or ccvx 4

--- R/ccvx_hist_post.R
+++ R/ccvx_hist_post.R
@@ -9,7 +9,12 @@
 #' @examples 
 #' ccvx.mod <- ccvx_build_jags()
 #' ccvx.samples <- ccvx_fit(ccvx.mod, doses = 0:4, mu.hat = c(1, 20, 50, 60, 65), std.err = rep(10, 5))
-#' par(mfrow=c(1,3))
+#' par(mfrow=c(1, 3))
+#' ccvx_hist_post(ccvx.samples)
+#' 
+#' #' ccvx.mod <- ccvx5_build_jags()
+#' ccvx.samples <- ccvx_fit(ccvx.mod, doses = 0:4, mu.hat = c(1, 20, 50, 60, 65), std.err = rep(10, 5))
+#' par(mfrow=c(2, 3))
 #' ccvx_hist_post(ccvx.samples)
 
 ccvx_hist_post <- function(ccvx.samples, cred.int.width = 0.9) {
@@ -18,7 +23,7 @@ ccvx_hist_post <- function(ccvx.samples, cred.int.width = 0.9) {
 
   # theta 0
   hist(ccvx.samples$jags.samples$theta_0, 
-       main = bquote(paste("Posterior for ", theta[0])), freq=FALSE, 
+       main = bquote("Posterior for" ~ theta[0] ~ "with" ~ .(100*cred.int.width) * "% C.I."), freq=FALSE, 
        xlab = bquote(theta[0]), cex.main = 1.3,
        cex.lab = 1.65, cex.axis = 1.3, breaks = 100)
   
@@ -32,13 +37,41 @@ ccvx_hist_post <- function(ccvx.samples, cred.int.width = 0.9) {
   
   abline(v = quantile(ccvx.samples$jags.samples$theta_1, probs = c(delta, cred.int.width + delta)), col = 2, lwd = 2.5)
   
-  # lambda
-  hist(ccvx.samples$jags.samples$lambda, 
-       main = bquote("Posterior for" ~ lambda ~ "with" ~ .(100*cred.int.width) * "% C.I."), 
-       freq = FALSE, cex.main = 1.3,
-       xlab = bquote(lambda), cex.lab = 1.65, cex.axis = 1.3, breaks = 100)
-
-  abline(v = quantile(ccvx.samples$jags.samples$lambda, probs = c(delta, cred.int.width + delta)), col = 2, lwd = 2.5)
+  if(any(grep('3-parameter concavex model', ccvx.samples$ccvx.mod))){ 
+    # lambda
+    hist(ccvx.samples$jags.samples$lambda, 
+         main = bquote("Posterior for" ~ lambda ~ "with" ~ .(100*cred.int.width) * "% C.I."), 
+         freq = FALSE, cex.main = 1.3,
+         xlab = bquote(lambda), cex.lab = 1.65, cex.axis = 1.3, breaks = 100)
+  
+    abline(v = quantile(ccvx.samples$jags.samples$lambda, probs = c(delta, cred.int.width + delta)), col = 2, lwd = 2.5)
+  }
+  
+  if(any(grep('5-parameter concavex spline model', ccvx.samples$ccvx.mod))){ 
+    # gamma_1
+    hist(ccvx.samples$jags.samples$gamma_1, 
+         main = bquote("Posterior for" ~ gamma[1] ~ "with" ~ .(100*cred.int.width) * "% C.I."), 
+         freq = FALSE, cex.main = 1.3,
+         xlab = bquote(gamma[1]), cex.lab = 1.65, cex.axis = 1.3, breaks = 100)
+    
+    abline(v = quantile(ccvx.samples$jags.samples$gamma_1, probs = c(delta, cred.int.width + delta)), col = 2, lwd = 2.5)
+    
+    # gamma_2
+    hist(ccvx.samples$jags.samples$gamma_2, 
+         main = bquote("Posterior for" ~ gamma[2] ~ "with" ~ .(100*cred.int.width) * "% C.I."), 
+         freq = FALSE, cex.main = 1.3,
+         xlab = bquote(gamma[2]), cex.lab = 1.65, cex.axis = 1.3, breaks = 100)
+    
+    abline(v = quantile(ccvx.samples$jags.samples$gamma_2, probs = c(delta, cred.int.width + delta)), col = 2, lwd = 2.5)
+    
+    # alpha
+    hist(ccvx.samples$jags.samples$alpha, 
+         main = bquote("Posterior for" ~ alpha ~ "with" ~ .(100*cred.int.width) * "% C.I."), 
+         freq = FALSE, cex.main = 1.3,
+         xlab = bquote(alpha), cex.lab = 1.65, cex.axis = 1.3, breaks = 100)
+    
+    abline(v = quantile(ccvx.samples$jags.samples$alpha, probs = c(delta, cred.int.width + delta)), col = 2, lwd = 2.5)
+  }
 }
 
 
