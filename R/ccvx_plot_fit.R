e877dc01dcdf794b6e40831bc7b8fd7df4f087c7
Paul Manser <paulmanser26@gmail.com>
2017-11-15 09:27:41 -0800

    generalize to ccvx or ccvx5

--- R/ccvx_plot_fit.R
+++ R/ccvx_plot_fit.R
@@ -16,6 +16,12 @@
 #' par(mfrow=c(1,2))
 #' ccvx_plot_fit(ccvx.samples, placebo.adjusted = FALSE)
 #' ccvx_plot_fit(ccvx.samples, placebo.adjusted = TRUE)
+#'
+#' ccvx.mod <- ccvx_build_jags()
+#' ccvx.samples <- ccvx_fit(ccvx.mod, doses = 0:4, mu.hat = c(1, 20, 50, 60, 65), std.err = rep(10, 5))
+#' par(mfrow=c(1,2))
+#' ccvx_plot_fit(ccvx.samples, placebo.adjusted = FALSE)
+#' ccvx_plot_fit(ccvx.samples, placebo.adjusted = TRUE)
 
 ccvx_plot_fit <- function(ccvx.samples, placebo.adjusted = FALSE,
                           title = "Concavex Model Fit", xlab = "Dose", ylab = "Effect", cred.int.width = .9) {
@@ -25,22 +31,17 @@ ccvx_plot_fit <- function(ccvx.samples, placebo.adjusted = FALSE,
   dose.range.orig <- seq(0, max(ccvx.samples$dose), length.out = 250)
   orig.ind <- sapply(ccvx.samples$dose, function(x) which.min(abs(dose.range.orig - x)))
   
-  # compute median parameter estimates
-  theta_0_median <- median(ccvx.samples$jags.samples$theta_0)
-  theta_1_median <- median(ccvx.samples$jags.samples$theta_1)
-  lambda_median <- median(ccvx.samples$jags.samples$lambda)
-  
   # compute credible interval bounds, plot points, and standard errors
-  pci.ul <- pci.ll <- numeric(length(dose.range.orig))
+  y <- pci.ul <- pci.ll <- numeric(length(dose.range.orig))
   delta <- (1 - cred.int.width)/2
   
   if (!placebo.adjusted){
     for(ii in 1:length(pci.ul)) {
-      quantiles <- quantile(unlist(ccvx.samples$jags.samples$mu.tilde[ii, , ]), probs = c(delta, cred.int.width + delta))
+      quantiles <- quantile(unlist(ccvx.samples$jags.samples$mu.tilde[ii, , ]), probs = c(delta, .5, cred.int.width + delta))
       pci.ll[ii] <- quantiles[1]
-      pci.ul[ii] <- quantiles[2]
+      y[ii] <- quantiles[2]
+      pci.ul[ii] <- quantiles[3]
     }
-    y <- theta_0_median + theta_1_median * (((((1-lambda_median)/2)^2 + lambda_median)/(((1-lambda_median)/2)^2 + lambda_median*dose.range))*dose.range)
     y.vals <- ccvx.samples$mu.hat
     x.vals <- ccvx.samples$doses
     std.err <- ccvx.samples$std.err
@@ -49,11 +50,11 @@ ccvx_plot_fit <- function(ccvx.samples, placebo.adjusted = FALSE,
   
   if (placebo.adjusted){
     for(ii in 1:length(pci.ul)) {
-      quantiles <- quantile(unlist(ccvx.samples$jags.samples$trt.post[ii, , ]), probs = c(delta, cred.int.width + delta))
+      quantiles <- quantile(unlist(ccvx.samples$jags.samples$trt.post[ii, , ]), probs = c(delta, .5, cred.int.width + delta))
       pci.ll[ii] <- quantiles[1]
-      pci.ul[ii] <- quantiles[2]
+      y[ii] <- quantiles[2]
+      pci.ul[ii] <- quantiles[3]
     }
-    y <- theta_1_median * (((((1-lambda_median)/2)^2 + lambda_median)/(((1-lambda_median)/2)^2 + lambda_median*dose.range))*dose.range)
     y.vals <- ccvx.samples$mu.hat[-1] - ccvx.samples$mu.hat[1]
     x.vals <- ccvx.samples$doses[-1]
     std.err <- sqrt(ccvx.samples$std.err[1]^2 + (ccvx.samples$std.err[-1]^2))
